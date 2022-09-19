697c8a3f70f81fc1b6b7e5a8192da75f48d1044d
victorvijay <vijayvj1210@gmail.com>
2022-09-19 21:26:21 +0530

    cucumber report integration

--- Karate/src/test/java/testdemo/TestRunner.java
+++ Karate/src/test/java/testdemo/TestRunner.java
@@ -1,6 +1,20 @@
 package testdemo;
-
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
 
 public class TestRunner {
 	
@@ -13,6 +27,8 @@ public class TestRunner {
 	Karate deleteAPItest() {
 		return Karate.run("delete").relativeTo(getClass());
 	}
+
+   
 
 	
 }
