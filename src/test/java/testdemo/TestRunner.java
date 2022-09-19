26e3c9e29849585dfcfcb056df4e7ecf23931398
victorvijay <vijayvj1210@gmail.com>
2022-09-19 21:53:35 +0530

    runner changes

--- src/test/java/testdemo/TestRunner.java
+++ src/test/java/testdemo/TestRunner.java
@@ -1,34 +1,42 @@
 package testdemo;
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
-import org.apache.commons.io.FileUtils;
-import static org.junit.Assert.*;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-
 
 public class TestRunner {
 	
-	@Karate.Test
-	Karate getAPItest() {
-		return Karate.run("getAPI").relativeTo(getClass());
-	}
 	
-	@Karate.Test
-	Karate deleteAPItest() {
-		return Karate.run("delete").relativeTo(getClass());
-	}
-
-   
-
 	
+	@Test
+	   public void testParallel() {
+	       Results results = Runner.path("classpath:testdemo/getAPI.feature")
+	               .outputCucumberJson(true)
+	               .karateEnv("karate demo")
+	               .parallel(1);
+	       generateReport(results.getReportDir());
+	       assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+	   }
+
+	   public static void generateReport(String karateOutputPath) {
+	       Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+	       List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+	       jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+	       Configuration config = new Configuration(new File("report"), "karate demo");
+	       ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+	       reportBuilder.generateReports();
+	   }
+
+
 }
